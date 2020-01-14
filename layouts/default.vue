5fa61ed00c2e0c1270dc9c4743e3bfd38de4ca05
Bob Walsh <bob.walsh@47hats.com>
2020-01-14 07:39:25 -0800

    login/logout working!

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,8 @@
     <div class="main">
       <div class="main-links">
         <nuxt-link to="/">Home Page</nuxt-link>|
-        <nuxt-link to="login">Login</nuxt-link>|
+        <div @click="logout" v-if="loggedIn" class="logout-link">Logout</div>
+        <nuxt-link to="login" v-else>Login</nuxt-link>|
         <nuxt-link to="secret">Secret</nuxt-link>
       </div>
       <nuxt />
@@ -11,7 +12,50 @@
   </div>
 </template>
 
+<script>
+import * as firebase from 'firebase/app'
+import 'firebase/auth'
+export default {
+  mounted() {
+    this.setupFirebase()
+  },
+  data() {
+    return {
+      loggedIn: false
+    }
+  },
+  methods: {
+    logout() {
+      alert('test')
+    },
+    setupFirebase() {
+      firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+          console.log('loggedin')
+          this.loggedIn = true
+        } else {
+          this.loggedIn = false
+        }
+      })
+    },
+    logout() {
+      firebase
+        .auth()
+        .signOut()
+        .then(() => {
+          this.$router.push('/')
+        })
+    }
+  }
+}
+</script>
 <style>
+.logout-link {
+  cursor: pointer;
+  text-decoration: underline;
+  color: #551a8b;
+}
+
 .main {
   margin: 30px;
   display: flex;
